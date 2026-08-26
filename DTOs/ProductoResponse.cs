4bdb343e30020b1e930f34e119a29ad565acbdd9
Meingel <meingel@outlook.com>
2026-08-25 20:41:44 -0500

    fix in message

--- DTOs/ProductoResponse.cs
+++
@@ -1,4 +0,0 @@
-namespace ProductosApiNet8.DTOs;
-
-// Devuelvo únicamente los datos públicos del producto.
-public record ProductoResponse(int Id, string Nombre, string? Descripcion, decimal Precio);
